f26729805f112b1f421b491687dc5ec5b44b12af
littler4 <littler4@wwu.edu>
2019-05-29 14:52:28 -0700

    fixed column tracking bug and real literal bug

--- src/main.c
+++ src/main.c
@@ -7,16 +7,18 @@
 #include "print_tree.h"
 
 int main(int argc, char* argv[]) {
-  int opt;
-  struct option long_opts[] = {
-    { "--help",   no_argument,       NULL, 'h' },
-    { "--output", required_argument, NULL, 'o' }
-  };
+//  int opt;
+//  struct option long_opts[] = {
+//    { "--help",   no_argument,       NULL, 'h' },
+//    { "--output", required_argument, NULL, 'o' }
+//  };
 
-  while (1) {
-    opt = getopt_long(argc, argv, "", long_opts, NULL);
-    //TODO
-  }
+//  while (1) {
+//    opt = getopt_long(argc, argv, "", long_opts, NULL);
+//
+//    switch (opt) {
+//    }
+//  }
 
   // lex
   ll_t tokens = lex(argv[1]);

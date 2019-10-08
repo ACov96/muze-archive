1f4e5ec3b131bccc074f3aadee598eeb53bad5dc
Alexander James Covington <covinga@LabImage2016-17>
2019-10-08 09:51:05 -0700

    Saving place to go fix parser

--- src/codegen.c
+++ src/codegen.c
@@ -1,7 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "codegen.h"
 #include "ast.h"
 
+unsigned int curr_label = 0;
+
+char* gen_label() {
+  // TODO: Make this malloc dynamic
+  char *label = malloc(16);
+  sprintf(label, "M%d", curr_label);
+  printf("GENERATED LABEL: %s\n", label);
+  curr_label++;
+  return label;
+}
+
 char* codegen(root_t root) {
+  gen_label();
   return "NOTHING";
 }
